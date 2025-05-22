0ccf4d531700712e29600dc183f47ba821ef3cfb
Azat Zagrutdinov <azatzagrutdinov@yandex.ru>
2025-05-22 23:31:01 +0300

    feat: создана ф-ий createOffer, getFullOffer , Доработка функции getOffers

--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
-import { getAllOffers } from "../controllers/offerController.js";
+import {
+  createOffer,
+  getAllOffers,
+  getFullOffer,
+} from "../controllers/offerController.js";
+import { upload } from "../middleware/upload.js";
 
 const offerRouter = new Router();
 
 offerRouter.get("/offers", getAllOffers);
+offerRouter.post(
+  "/offers",
+  upload.fields([
+    { name: "previewImage", maxCount: 1 },
+    { name: "photos", maxCount: 6 },
+  ]),
+  createOffer
+);
+
+offerRouter.get("/offers/:id", getFullOffer);
 
 export default offerRouter;
