1e34c48ecc002361fc589d3a0a47c06c5840536b
Azat Zagrutdinov <azatzagrutdinov@yandex.ru>
2025-05-30 01:09:16 +0300

    fix: add / for path

--- rental-service/server/routes/userRoutes.js
+++ rental-service/server/routes/userRoutes.js
@@ -12,6 +12,6 @@ const router = new Router();
 router.post("/registration", upload.single("avatar"), registration);
 router.post("/login", login);
 router.get("/login", authenticateToken, checkAuth);
-router.delete("logout", logout);
+router.delete("/logout", logout);
 
 export default router;
